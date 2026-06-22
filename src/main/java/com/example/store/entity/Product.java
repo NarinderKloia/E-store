88a050099cb14780392224a272a571f54fd8f567
Narinder Kloia <navirajput5566@gmail.com>
2026-06-23 01:13:06 +0530

    product files created

---
+++ src/main/java/com/example/store/entity/Product.java
@@ -0,0 +1,12 @@
+package com.example.store.entity;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private Long id;
+    private String name;
+    private String description;
+    private String category;
+    private BigDecimal price;
+    private Integer stock;
+}
