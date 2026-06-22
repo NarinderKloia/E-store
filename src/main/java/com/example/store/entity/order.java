44d46f26e4c7930da4df1a7dd9729cac56ab70f1
Narinder Kloia <navirajput5566@gmail.com>
2026-06-23 03:07:45 +0530

    relation done

--- src/main/java/com/example/store/entity/order.java
+++ src/main/java/com/example/store/entity/order.java
@@ -2,12 +2,14 @@ package com.example.store.entity;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
@@ -17,7 +19,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "Order")
+@Table(name = "orders")
 @Getter
 @Setter
 @AllArgsConstructor
@@ -28,20 +30,23 @@ public class order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(table = "customer_name", nullable = false)
+    @Column(name = "customer_name", nullable = false)
     private String customerName;
 
     @Email
-    @Column(table = "customer_email", nullable = false)
+    @Column(name = "customer_email", nullable = false)
     private String customerEmail;
 
     @Column(nullable = false)
     private String status;
 
-    @Column(table = "total_price",nullable = false)
+    @Column(name = "total_price",nullable = false)
     private BigDecimal totalPrice;
 
-    @Column(table = "created_at",nullable = false)
+    @OneToMany(mappedBy = "order")
+    private List<OrderItems> orderitems;
+
+    @Column(name = "created_at",nullable = false)
     private LocalDateTime createdAt;
 
     @PrePersist
