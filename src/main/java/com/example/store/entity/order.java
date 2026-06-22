8390a42ac367e02bb0d953686a2eb0f298d746db
Narinder Kloia <navirajput5566@gmail.com>
2026-06-23 02:16:41 +0530

    order.java completed

---
+++ src/main/java/com/example/store/entity/order.java
@@ -0,0 +1,52 @@
+package com.example.store.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Order")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(table = "customer_name", nullable = false)
+    private String customerName;
+
+    @Email
+    @Column(table = "customer_email", nullable = false)
+    private String customerEmail;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(table = "total_price",nullable = false)
+    private BigDecimal totalPrice;
+
+    @Column(table = "created_at",nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist(){
+        this.createdAt= LocalDateTime.now();
+    }
+}
